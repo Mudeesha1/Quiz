85cd5c56d048699e52e6894a60cb7ad6c6308abb
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2026-07-13 09:56:02 +0530

    feat: add user route definitions and file upload system documentation

--- backend/modules/users/users.routes.js
+++ backend/modules/users/users.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const registerUser = require("./controllers/register.controller");
 const loginUser = require("./controllers/login.controller");
+const refreshController = require("./controllers/refresh.controller");
 const { getUserProfileWithRank, updateProfile, updateReview } = require("./controllers/profile.controller");
 const forgetPassword = require("./controllers/forgetPassword.controller");
 const resetPassword = require("./controllers/resetPassword.controller");
@@ -13,6 +14,7 @@ usersRoutes.post("/register", registerUser);
 usersRoutes.post("/login", loginUser);
 usersRoutes.post("/forget-password", forgetPassword);
 usersRoutes.post("/reset-password", resetPassword);
+usersRoutes.post("/refresh", refreshController);
 
 // Protected routes for registered users
 usersRoutes.get("/profile", requireUser, getUserProfileWithRank);
